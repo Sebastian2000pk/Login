00eb8b0846de93e9196d529563a7201a1c945929
Sebastian2000pk <juochoas@unal.edu.co>
2021-01-17 12:26:37 -0500

    Primera eli

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+const PORT = '3000';
+
+app.use('/', require('./routes/index'));
+app.use('/user', require('./routes/user'));
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
