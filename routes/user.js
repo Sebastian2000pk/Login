8eb22ec840adc3de576109bf72784ab683f62c7b
Sebastian2000pk <71113419+Sebastian2000pk@users.noreply.github.com>
2021-01-18 17:19:48 -0500

    Segunda corregida

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 //login Page
-router.get('/login', (req, res) => res.send('Login'));
+router.get('/login', (req, res) => res.render('Login'));
 
 // Register Page
-router.get('/register', (req, res) => res.send('Register'));
+router.get('/register', (req, res) => res.render('Register'));
 
 module.exports = router;
